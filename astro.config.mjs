c5de8e9c1c566b76ed752ccab9160737fe8d6ba6
Clapboiz <phamconglap03@gmail.com>
2024-08-30 23:35:42 +0700

    update page

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://Clapboiz.github.io/",
+  site: "https://Clapboiz.github.io",
   base: "/",
   trailingSlash: "always",
   integrations: [
