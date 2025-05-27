2c8f032dcfeb5d0a5ac3d2a83ad061e4acab5390
Clapboiz <phamconglap03@gmail.com>
2025-05-28 01:16:17 +0700

    GLOBAL-CYBER-SKILLS-BENCHMARK-CTF-2025

--- src/config.ts
+++ src/config.ts
@@ -1,82 +1,159 @@
+// import type {
+//   LicenseConfig,
+//   NavBarConfig,
+//   ProfileConfig,
+//   SiteConfig,
+// } from './types/config'
+// import { LinkPreset } from './types/config'
+
+// export const siteConfig: SiteConfig = {
+//   title: 'Clapboiz',
+//   subtitle: 'Demo Site',
+//   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
+//   themeColor: {
+//     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+//     fixed: false,     // Hide the theme color picker for visitors
+//   },
+//   banner: {
+//     enable: true,
+//     src: 'assets/images/banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+//     position: 'center', // Equivalent to object-position, defaults center
+//     credit: {
+//       enable: false,         // Display the credit text of the banner image
+//       text: '',              // Credit text to be displayed
+//       url: ''                // (Optional) URL link to the original artwork or artist's page
+//     }
+//   },
+//   toc: {
+// 		enable: true, // Display the table of contents on the right side of the post
+// 		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
+// 	},
+//   favicon: [    // Leave this array empty to use the default favicon
+//     // {
+//     //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
+//     //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+//     //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+//     // }
+//   ]
+// }
+
+// export const navBarConfig: NavBarConfig = {
+//   links: [
+//     LinkPreset.Home,
+//     LinkPreset.Archive,
+//     LinkPreset.About,
+//     {
+//       name: 'GitHub',
+//       url: 'https://github.com/Clapboiz',     // Internal links should not include the base path, as it is automatically added
+//       external: true,                               // Show an external link icon and will open in a new tab
+//     },
+//   ],
+// }
+
+// export const profileConfig: ProfileConfig = {
+//   avatar: 'assets/images/logo_clap.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+//   name: 'Phạm Lập',
+//   bio: 'Penetration Tester',
+//   links: [
+//     {
+//       name: 'Twitter',
+//       icon: 'fa6-brands:linkedin',       // Visit https://icones.js.org/ for icon codes
+//                                         // You will need to install the corresponding icon set if it's not already included
+//                                         // `pnpm add @iconify-json/<icon-set-name>`
+//       url: 'https://www.linkedin.com/in/clapboiz/',
+//     },
+//     // {
+//     //   name: 'Steam',
+//     //   icon: 'fa6-brands:steam',
+//     //   url: 'https://store.steampowered.com',
+//     // },
+//     {
+//       name: 'GitHub',
+//       icon: 'fa6-brands:github',
+//       url: 'https://github.com/Clapboiz',
+//     },
+//   ],
+// }
+
+// export const licenseConfig: LicenseConfig = {
+//   enable: true,
+//   name: 'CC BY-NC-SA 4.0',
+//   url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
+// }
 import type {
-  LicenseConfig,
-  NavBarConfig,
-  ProfileConfig,
-  SiteConfig,
-} from './types/config'
-import { LinkPreset } from './types/config'
+	LicenseConfig,
+	NavBarConfig,
+	ProfileConfig,
+	SiteConfig,
+} from "./types/config";
+import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-  title: 'Clapboiz',
-  subtitle: 'Demo Site',
-  lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
-  themeColor: {
-    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: false,     // Hide the theme color picker for visitors
-  },
-  banner: {
-    enable: true,
-    src: 'assets/images/banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-    position: 'center', // Equivalent to object-position, defaults center
-    credit: {
-      enable: false,         // Display the credit text of the banner image
-      text: '',              // Credit text to be displayed
-      url: ''                // (Optional) URL link to the original artwork or artist's page
-    }
-  },
-  toc: {
+	title: "Clapboiz",
+	subtitle: "Demo Site",
+	lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
+	themeColor: {
+		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		fixed: false, // Hide the theme color picker for visitors
+	},
+	banner: {
+		enable: true,
+		src: "assets/images/banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+		credit: {
+			enable: false, // Display the credit text of the banner image
+			text: "", // Credit text to be displayed
+			url: "", // (Optional) URL link to the original artwork or artist's page
+		},
+	},
+	toc: {
 		enable: true, // Display the table of contents on the right side of the post
 		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
 	},
-  favicon: [    // Leave this array empty to use the default favicon
-    // {
-    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-    // }
-  ]
-}
+	favicon: [
+		// Leave this array empty to use the default favicon
+	],
+};
 
 export const navBarConfig: NavBarConfig = {
-  links: [
-    LinkPreset.Home,
-    LinkPreset.Archive,
-    LinkPreset.About,
-    {
-      name: 'GitHub',
-      url: 'https://github.com/Clapboiz',     // Internal links should not include the base path, as it is automatically added
-      external: true,                               // Show an external link icon and will open in a new tab
-    },
-  ],
-}
+	links: [
+		LinkPreset.Home,
+		LinkPreset.Archive,
+		LinkPreset.About,
+		{
+			name: "GitHub",
+			url: "https://github.com/Clapboiz", // Internal links should not include the base path, as it is automatically added
+			external: true, // Show an external link icon and will open in a new tab
+		},
+	],
+};
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/logo_clap.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Phạm Lập',
-  bio: 'Penetration Tester',
-  links: [
-    {
-      name: 'Twitter',
-      icon: 'fa6-brands:linkedin',       // Visit https://icones.js.org/ for icon codes
-                                        // You will need to install the corresponding icon set if it's not already included
-                                        // `pnpm add @iconify-json/<icon-set-name>`
-      url: 'https://www.linkedin.com/in/clapboiz/',
-    },
-    // {
-    //   name: 'Steam',
-    //   icon: 'fa6-brands:steam',
-    //   url: 'https://store.steampowered.com',
-    // },
-    {
-      name: 'GitHub',
-      icon: 'fa6-brands:github',
-      url: 'https://github.com/Clapboiz',
-    },
-  ],
-}
+	avatar: "assets/images/logo_clap.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	name: "Phạm Lập",
+	bio: "Penetration Tester",
+	links: [
+		{
+			name: "LinkedIn",
+			icon: "fa6-brands:linkedin", // Icon code, cần cài bộ icon nếu chưa có
+			url: "https://www.linkedin.com/in/clapboiz/",
+		},
+		// Nếu muốn bật thêm Steam, bỏ comment đoạn dưới
+		// {
+		//   name: "Steam",
+		//   icon: "fa6-brands:steam",
+		//   url: "https://store.steampowered.com",
+		// },
+		{
+			name: "GitHub",
+			icon: "fa6-brands:github",
+			url: "https://github.com/Clapboiz",
+		},
+	],
+};
 
 export const licenseConfig: LicenseConfig = {
-  enable: true,
-  name: 'CC BY-NC-SA 4.0',
-  url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
-}
+	enable: true,
+	name: "CC BY-NC-SA 4.0",
+	url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+};
