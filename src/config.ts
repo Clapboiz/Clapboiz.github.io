beea0b908468c29f209e916b215b2e0630f7e3ca
Clapboiz <phamconglap03@gmail.com>
2026-03-27 22:46:11 +0700

    full content

--- src/config.ts
+++ src/config.ts
@@ -90,7 +90,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "Clapboiz",
-	subtitle: "Demo Site",
+	subtitle: "Phạm Lập",
 	lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
